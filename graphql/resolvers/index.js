e013eefa7627ed281122b4610fea6d6461ca8f63
fahiz <fahizkppullara@gmail.com>
2021-07-18 11:05:15 +0530

    final Touch

---
+++ graphql/resolvers/index.js
@@ -0,0 +1,20 @@
+const clientsResolvers = require('./clients');
+const usersResolvers = require('./users');
+const messagesResolvers = require('./messages');
+
+module.exports ={
+    Client:{
+        messageCount:(parent) => parent.messages.length
+    },
+    Query: {
+    ...clientsResolvers.Query
+    },
+    Mutation:{
+        ...usersResolvers.Mutation,
+        ...clientsResolvers.Mutation,
+        ...messagesResolvers.Mutation,
+    },
+    Subscription:{
+        ...clientsResolvers.Subscription,
+    }
+}
